742f18b0c4eded5d4dcc28facae4f8d366b42684
abhisheksaral <abhisheksaral17@gmail.com>
2021-07-30 02:23:19 -0700

    Custom Navigation and Platform Specific Icons

--- lib/components/lists/recent_course_list.dart
+++ lib/components/lists/recent_course_list.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:designcode/components/cards/recent_course_card.dart';
 import 'package:designcode/model/course.dart';
+import 'package:designcode/screens/course_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:blur/blur.dart';
 
@@ -19,7 +20,7 @@ class _RecentCourseListState extends State<RecentCourseList> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: recentCourses.map(
-        (course) {
+            (course) {
           var index = recentCourses.indexOf(course);
           return Container(
             width: 7,
@@ -45,18 +46,32 @@ class _RecentCourseListState extends State<RecentCourseList> {
           width: double.infinity,
           child: PageView.builder(
             itemBuilder: (context, index) {
-              return Blur(
-                blur: currentPage == index ? 0 : 5,
-                colorOpacity: 0,
-                alignment: Alignment.centerRight,
-                child: Opacity(
-                    opacity: currentPage == index ? 1.0 : 0.5,
-                    child: RecentCourseCard(course: recentCourses[index])),
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CourseScreen(course: recentCourses[index],),
+                      fullscreenDialog: true,
+                    ),
+                  );
+                },
+                child: Blur(
+                  blur: currentPage == index ? 0 : 5,
+                  colorOpacity: 0,
+                  alignment: Alignment.centerRight,
+                  child: Opacity(
+                      opacity: currentPage == index ? 1.0 : 0.5,
+                      child: RecentCourseCard(course: recentCourses[index])
+                  ),
+                ),
               );
             },
             itemCount: recentCourses.length,
             controller: PageController(
-                initialPage: 0, viewportFraction: Platform.isIOS ? 0.63 : 0.67),
+                initialPage: 0,
+                viewportFraction: Platform.isIOS ? 0.63 : 0.67
+            ),
             onPageChanged: (index) {
               setState(() {
                 currentPage = index;
--- lib/model/sidebar.dart
+++ lib/model/sidebar.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SideBarItem {
@@ -33,7 +36,7 @@ var sidebarItems = [
         end: Alignment.bottomRight,
         colors: [Color(0xFFFA7d75), Color(0xFFC23D61)]),
     icon: Icon(
-      Icons.library_books,
+      Platform.isAndroid ? Icons.library_books : CupertinoIcons.book_solid,
       color: Colors.white,
     ),
   ),
@@ -56,7 +59,7 @@ var sidebarItems = [
       colors: [Color(0xFF4E62CC), Color(0xFF202A78)],
     ),
     icon: Icon(
-      Icons.settings,
+      Platform.isAndroid ? Icons.settings : CupertinoIcons.settings_solid,
       color: Colors.white,
     ),
   ),
